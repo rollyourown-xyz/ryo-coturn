144a7e8c55390118a9502c3a00b65207d0f6f86b
xyzroller <xyzroller@rollyourown.xyz>
2021-07-19 22:24:39 +0200

    Added setup firewall role

---
+++ image-build/playbooks/roles/set-up-firewall/files/iptables-rules.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+IPT="/sbin/iptables"
+
+# By default, drop everything except outgoing traffic
+$IPT -P INPUT DROP
+$IPT -P FORWARD DROP
+$IPT -P OUTPUT ACCEPT
+
+
+## General Rules
+################
+
+# Allow incoming and outgoing for loopback interfaces
+$IPT -A INPUT -i lo -j ACCEPT
+$IPT -A OUTPUT -o lo -j ACCEPT
+
+# ICMP rule
+$IPT -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
+
+# Allow established connections:
+$IPT -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+
+## Consul Client Ports
+######################
+
+# Allow Consul LAN Serf on TCP and UDP Ports 8301
+$IPT -A INPUT -p udp -m udp --dport 8301 -j ACCEPT
+$IPT -A INPUT -p tcp -m tcp --dport 8301 -j ACCEPT
+
+
+## Coturn Rules
+###############
+
+# Allow coturn UDP and TCP listener port 3478
+$IPT -A INPUT -p udp -m udp --dport 3478 -j ACCEPT
+$IPT -A INPUT -p tcp -m tcp --dport 3478 -j ACCEPT
+$IPT -A INPUT -p udp -m udp --dport 5349 -j ACCEPT
+$IPT -A INPUT -p tcp -m tcp --dport 5349 -j ACCEPT
+
+# Allow coturn UDP relay port range 50000-51000
+$IPT -A INPUT -p udp -m udp --dport 50000:51000 -j ACCEPT
