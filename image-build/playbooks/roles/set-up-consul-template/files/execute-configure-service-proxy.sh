dc087988e41916d2f04f821e79a6a42fa38d32bf
xyzroller <xyzroller@rollyourown.xyz>
2021-09-05 18:39:20 +0200

    Added coturn restart after service-proxy config

--- image-build/playbooks/roles/set-up-consul-template/files/execute-configure-service-proxy.sh
+++ image-build/playbooks/roles/set-up-consul-template/files/execute-configure-service-proxy.sh
@@ -5,3 +5,9 @@ chmod 775 /usr/local/bin/configure-service-proxy.sh
 
 # Execute configure-service-proxy.sh
 /usr/local/bin/configure-service-proxy.sh
+
+# Wait 60s for certbot certificates to be obtained
+sleep 60
+
+# Restart coturn
+/usr/local/bin/restart-coturn.sh
